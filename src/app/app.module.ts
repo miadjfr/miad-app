4893d175b5464009daafbc99a73a8fd6bbf5f2c9
Miad jfr <miad,jafari@live.com>
2024-04-25 10:58:43 +0330

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
